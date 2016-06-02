6d139c6748e484b163a35e270f805b484f4fc91d
GabrielPontin <gabrielpontin01@gmail.com>
2016-06-01 22:10:22 -0300

    selecionado e iterator

--- src/br/univel/print/PessoaJRDataSource.java
+++ src/br/univel/print/PessoaJRDataSource.java
@@ -1,5 +1,6 @@
 package br.univel.print;
 
+import java.util.Iterator;
 import java.util.List;
 
 import br.univel.Pessoa;
@@ -10,20 +11,35 @@ import net.sf.jasperreports.engine.JRField;
 public class PessoaJRDataSource implements JRDataSource {
 
 	private List<Pessoa> lista;
+	private Pessoa selecionado;
+	private Iterator<Pessoa> iterator;
 
 	public PessoaJRDataSource(List<Pessoa> lista) {
 		this.lista = lista;
+		this.iterator = lista.iterator();
 	}
 
 	@Override
 	public Object getFieldValue(JRField arg0) throws JRException {
-		// TODO Auto-generated method stub
-		return null;
+		
+		if("id".equals(arg0.getName())){
+			return selecionado.getId();
+			
+		}else if("nome".equals(arg0.getName())){
+			return selecionado.getNome();
+			
+		}else if(arg0.getName().equals("Telefone")){
+			return selecionado.getTelefone();
+		}
+		return "Nao Identificado";
 	}
 
 	@Override
 	public boolean next() throws JRException {
-		// TODO Auto-generated method stub
+		if(iterator.hasNext()){
+			selecionado = iterator.next();
+			return true;
+		}
 		return false;
 	}
 
